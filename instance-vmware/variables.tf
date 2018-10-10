176af4a2f4314e120f3a6ed5bec8ff7b7439e8ab
lorenzocomotti <lorenzo.comotti97@gmail.com>
2018-10-10 15:50:51 +0200

    Add WMware module

---
+++ instance-vmware/variables.tf
@@ -0,0 +1,50 @@
+variable "hostname" {
+  default = "automium-managed-vm"
+}
+
+variable "domain" {
+  default = "automium.internal"
+}
+
+variable "quantity" {
+  default = 1
+}
+
+variable "cpus" {
+  default = 1
+}
+
+variable "memory" {
+  default = 512
+}
+
+variable "memory_reservation" {
+  default = 256
+}
+
+variable "name" {
+}
+
+variable "vmware_template" {
+  default = "Ubuntu-16.04"
+}
+
+variable "vmware_datastore" {
+}
+
+variable "vmware_network_name" {
+}
+
+variable "vmware_datacenter" {
+}
+
+variable "vmware_resource_pool"{
+}
+
+variable "tags" {
+  type = "map"
+  default = {
+    role = "generic"
+    status = "generic"
+  }
+}
