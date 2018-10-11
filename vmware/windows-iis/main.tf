db5dd28863f6e8116ab3e62624d61b679e5e3596
lorenzocomotti <lorenzo.comotti97@gmail.com>
2018-10-11 13:55:56 +0200

    Add Windows IIS module for VMware

---
+++ vmware/windows-iis/main.tf
@@ -0,0 +1,22 @@
+module "windows-istance" {
+  source = "github.com/entercloudsuite/terraform-modules/vmware/instance"
+  name = "${var.name}"
+  cpus = "${var.cups}"
+  memory = "${var.memory}"
+  vmware_template = "Windows-2016"
+  quantity = "${var.quantity}"
+  vmware_network_name = "${var.network_name}"
+  vmware_datastore = "${var.vmware_datastore}"
+  vmware_network_name = "${var.vmware_network_name}"
+  vmware_datacenter = "${var.vmware_datacenter}"
+  vmware_resource_pool = "${var.vmware_resource_pool}"
+  userdata = "${data.template_file.cloud-config.*.rendered}"
+  }
+
+data "template_file" "cloud-config" {
+  template = "${file("${path.module}/cloud-config.ps1")}"
+  count = "${var.quantity}"
+  vars {
+    password = "${var.password}"
+  }
+}
